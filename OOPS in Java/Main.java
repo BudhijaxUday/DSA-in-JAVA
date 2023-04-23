bb8168c3de1a054e253690c37c2a314108d85b88
Uday Budhija <udaybudhija26@gmail.com>
2023-04-23 16:53:39 +0530

    Add files via upload

---
+++ OOPS in Java/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        Store 5 roll nos
+
+        int[] numbers = new int[5];
+
+//        Store 5 names
+
+        String[] names = new String[5];
+
+//        Data of 5 students: {roll no. , name , marks}
+
+        int[] roll_No = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+
+        Students[] students = new Students[5];
+    }
+}
+
+//    Create a class
+
+ class Students {
+        int[] roll_No = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+    }
+
